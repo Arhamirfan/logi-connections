b3c4f1ee1a50701585a395a534e8160581dc83fb
Arhamirfan <67455750+Arhamirfan@users.noreply.github.com>
2022-09-12 16:50:37 +0500

    rehman

--- src/Model/Store/index.js
+++ src/Model/Store/index.js
@@ -2,6 +2,6 @@ import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 
 import rootReducer from "./Reducers";
-
+// sfdgdsfgdsfgdgfsf sdfsdfsd
 const store = createStore(rootReducer, applyMiddleware(thunk));
 export default store;
